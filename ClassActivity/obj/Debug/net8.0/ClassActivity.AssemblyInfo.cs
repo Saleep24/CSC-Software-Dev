78cbf838fae752afdd7087f45717008e03e50f98
Saleep Shrestha <saleep24@gmail.com>
2025-02-20 14:09:44 -0600

    file name changed

--- ClassActivity/obj/Debug/net8.0/ClassActivity.AssemblyInfo.cs
+++ ClassActivity/obj/Debug/net8.0/ClassActivity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassActivity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5038c790ec3f5927eb341ac8ba38f0d2493773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622dd5d20a7c3d0388e68d84c579e80fbf965c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassActivity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassActivity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
